02560647eb90c423cab8c12e6fefb8249d7ff684
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2022-09-12 16:49:33 +0700

    login dan register

---
+++ android/app/src/main/kotlin/com/example/samara_xposi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.samara_xposi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/register.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:samara_xposi/main.dart';
+
+void main() {
+  runApp(const Register());
+}
+
+class Register extends StatelessWidget {
+  const Register({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      home: const MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (BuildContext context) => const MyApp()));
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            child: const Text(
+              "Data Akun",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            autocorrect: false,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.account_circle),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                  width: 1.0,
+                ),
+              ),
+              filled: true,
+              fillColor: const Color.fromARGB(255, 219, 239, 255),
+              hintText: "Email",
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            keyboardType: TextInputType.visiblePassword,
+            autocorrect: false,
+            obscureText: true,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.lock),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                  width: 1.0,
+                ),
+              ),
+              filled: true,
+              fillColor: const Color.fromARGB(255, 219, 239, 255),
+              hintText: "Password",
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            child: const Text(
+              "Detail Informasi",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            keyboardType: TextInputType.text,
+            autocorrect: false,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.account_balance),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                  width: 1.0,
+                ),
+              ),
+              filled: true,
+              fillColor: const Color.fromARGB(255, 219, 239, 255),
+              hintText: "Nama Lengkap",
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            keyboardType: TextInputType.phone,
+            autocorrect: false,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.phone),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                  width: 1.0,
+                ),
+              ),
+              filled: true,
+              fillColor: const Color.fromARGB(255, 219, 239, 255),
+              hintText: "No Telepon",
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            keyboardType: TextInputType.datetime,
+            autocorrect: false,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.date_range),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                  width: 1.0,
+                ),
+              ),
+              filled: true,
+              fillColor: const Color.fromARGB(255, 219, 239, 255),
+              hintText: "Tanggal Lahir",
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+            onPressed: (() {}),
+            child: Text(
+              "Register",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: const Color.fromARGB(255, 131, 54, 255),
+              fixedSize: const Size(150, 50),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+            ),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
